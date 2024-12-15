23c507b1f8516c73a64c908bc730e784a522a94d
0abhishek <abhishek.singh6025@gmail.com>
2024-12-15 23:26:28 +0530

    demo application setup

---
+++ src/main/java/com/spring/cache/DemoProjectOnCachingMechanismApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectOnCachingMechanismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectOnCachingMechanismApplication.class, args);
+	}
+
+}
