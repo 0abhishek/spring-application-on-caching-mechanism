83dac26019be2075a317437e524b151f899df41a
0abhishek <abhishek.singh6025@gmail.com>
2024-12-16 00:09:16 +0530

    formatted code

--- src/main/java/com/spring/cache/dto/EmployeeDto.java
+++ src/main/java/com/spring/cache/dto/EmployeeDto.java
@@ -3,13 +3,13 @@ package com.spring.cache.dto;
 import lombok.Data;
 
 /**
-* @author Abhishek.Singh
-* @spring_version 2.4.1
-* @java_verison 1.8
-*/
+ * @author Abhishek.Singh
+ * @spring_version 2.4.1
+ * @java_verison 1.8
+ */
 @Data
 public class EmployeeDto {
-	
+
 	private Long empId;
 	private String empName;
 	private String empDesignation;
